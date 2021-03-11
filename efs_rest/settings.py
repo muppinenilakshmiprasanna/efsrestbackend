aeb15dbe96f2bbe7450f78ab652b915ff6ef241a
muppinenilakshmiprasanna <lpmuppineni26@gmail.com>
2021-03-11 15:04:04 -0600

    new changes

--- efs_rest/settings.py
+++ efs_rest/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!ntvzt-ysr%+5kdfk6*1swjlu+pjdn_)mt!++tdjzlut6yx(yx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
